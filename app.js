4acfbd2f01f1a7199b4b7231d92f24f8f785e2ad
DRS05 <79338608+DRS05@users.noreply.github.com>
2024-12-09 12:50:49 +0100

    Add rock paper scissors functionality
    Added the functionality to play rock paper scissors with the terminal
    in and the prompt. Press CTRL+SHIFT+J to see the outputs on the server
    console. getHumanChoice is used for getting the user input.
    getComputerChoice is for getting the randomised computer input.
    playRound is for evaluating the outcome of a rps round. playGame
    is for playing the game for 5 rounds. determineWinner is for
    determining which player has more points and the player with the most
    points is going to be the winner. printScore prints the score of the
    player and the computer in the console (web) / terminal (node).

---
+++ app.js
@@ -0,0 +1,88 @@
+"use strict";
+
+
+function getComputerChoice() {
+	const randomChoiceIndex = Math.floor(Math.random() * 3);
+
+	switch (randomChoiceIndex) {
+		case 0:
+			return "rock";
+		case 1:
+			return "paper";
+		case 2:
+			return "scissors";
+		default:
+			return "rock";
+	};
+};
+
+
+function getHumanChoice() {
+	let humanChoice;
+	do {
+		humanChoice = prompt("Rock, paper or scissors?").toLowerCase();
+	} while (!["rock", "paper", "scissors"].includes(humanChoice));
+
+	return humanChoice;
+}
+
+
+function playRound(humanChoice, computerChoice) {
+	if (humanChoice === computerChoice) {
+		console.log(`Draw! ${humanChoice} neither beats nor is beaten by ${computerChoice}.`);
+		return 0;
+	}
+	
+	if (humanChoice === "rock" && computerChoice === "paper") {
+		return -1;
+	} else if (humanChoice === "paper" && computerChoice === "scissors") {
+		return -1;
+	} else if (humanChoice === "scissors" && computerChoice === "rock") {
+		return -1;
+	} else {
+		return 1;
+	}
+}
+
+
+function printScore(humanScore, computerScore) {
+	console.log(`Human: ${humanScore} points | Computer ${computerScore} points`);
+}
+
+
+function determineWinner(humanScore, computerScore) {
+	if (humanScore === computerScore) {
+		console.log("It's a draw!");
+	} else if (humanScore > computerScore) {
+		console.log("You win!");
+	} else {
+		console.log("You lose!");
+	}
+}
+
+
+function playGame() {
+	let humanScore = 0;
+	let computerScore = 0;
+
+	for (let i = 0; i < 5; i++) {
+		const computerChoice = getComputerChoice();
+		const humanChoice = getHumanChoice();
+
+		const outcomeHuman = playRound(humanChoice, computerChoice);
+
+		if (outcomeHuman === -1) {
+			computerScore++;
+			console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+		} else if (outcomeHuman === 1) {
+			humanScore++;
+			console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+		}
+
+		printScore(humanScore, computerScore);
+	}
+
+	determineWinner(humanScore, computerScore);
+}
+
+playGame();
